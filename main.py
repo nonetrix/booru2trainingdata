1197e3d1cb67dbbe18d150b1b21a7e61f6cdfd59
fuggy <45698918+nonetrix@users.noreply.github.com>
2023-01-15 14:51:21 -0600

    Correct formatting, add TODOs

--- main.py
+++ main.py
@@ -2,24 +2,33 @@ import requests, cv2, math, os
 import numpy as np
 
 # Ask user for image save directory and store input
-print("Pick a directory to save images to, it must already exist: ")
+print('Pick a directory to save images to, it must already exist: ')
 image_save_directory = input()
 
+# TODO Check for API keys and tell the user to make the needed files
+
+# TODO ask the user what booru to use
+# print('Pick what booru service you would like to use: ')
+# print('1. Danbooru')
+# print('2. Gelbooru')
+# print('3. E621')
+# print('4. Yande.re')
+
 # TODO
-# print("Pick what booru service you would like to use: ")
+# print('Would you like to change the base URL you are using from the default? e.g. R34 instead of Gelbooru as the use the same API.')
 
-
-print("Are your images NSFW? (Y/n): ")
+# Ask the user if their training data should be tagged as NSFW
+print('Are your images NSFW? (Y/n): ')
 nsfw_choice = input()
-if(nsfw_choice.upper() == "Y"):
-        content_type = "nsfw"
-elif(nsfw_choice.upper() == "N"):
-        content_type = "sfw"
+if(nsfw_choice.upper() == 'Y'):
+        content_type = 'nsfw'
+elif(nsfw_choice.upper() == 'N'):
+        content_type = 'sfw'
 
 
 # Loop to continuously retrieve image IDs from user
 while True:
-    print("Enter a image ID for Danbooru:")
+    print('Enter a image ID for Danbooru:')
     id = input()
     
     # Retrieve post data from Danbooru API
