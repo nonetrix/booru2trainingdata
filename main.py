ead25db93fe7cddf7f21e0f84e3db6d7c123562f
fuggy <45698918+nonetrix@users.noreply.github.com>
2023-01-22 00:53:38 -0600

    Added rule34 support, inplemented changing URLs

--- main.py
+++ main.py
@@ -24,14 +24,15 @@ def get_image_danbooru(id):
     else:
         print('Request failed.')
 
-def get_image_gelbooru(id):
+def get_image_gelbooru(id, url):
     """
     Get image details from Gelbooru
     :param id: Image ID on Gelbooru
+    :param url: URL to instance
     :return: Dictionary containing image details
     """
     # Make request to Gelbooru API
-    response = requests.get('https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&id=' + id, headers=headers)
+    response = requests.get(url + '/index.php?page=dapi&s=post&q=index&json=1&id=' + id, headers=headers)
 
     # Check if request was successful
     if response.status_code == 200:
@@ -45,6 +46,28 @@ def get_image_gelbooru(id):
     else:
         print('Request failed.')
 
+def get_image_gelbooru_old(id, url):
+    """
+    Get image details from Gelbooru using older API
+    :param id: Image ID on Gelbooru
+    :param url: URL to instance
+    :return: Dictionary containing image details
+    """
+    # Make request to Gelbooru API
+    response = requests.get(url + '/index.php?page=dapi&s=post&q=index&json=1&id=' + id, headers=headers)
+
+    # Check if request was successful
+    if response.status_code == 200:
+        post = response.json()
+        return {
+            'file_url': post[0]['file_url'],
+            'file_ext': post[0]['file_url'][post[0]['file_url'].rfind('.'):],
+            'id': post[0]['id'],
+            'tag_string': post[0]['tags'],
+        }
+    else:
+        print('Request failed.')
+
 # Get directory to save images
 print('Pick a directory to save images to, it must already exist: ')
 image_save_directory = input()
@@ -53,18 +76,29 @@ image_save_directory = input()
 print('''What Booru service would you like to use?
 1. Danbooru(What most anime models are already trained on)
 2. Gelbooru(Tags can be diffrent so might not be as good, however has less censorship without Gold account)
-Pick a number 1..2: ''')
+3. Gelbooru old(same API as rule34 xxx as it seems to be on older version)
+Pick a number 1..3: ''')
 booru_choice = int(input())
 
 # If user chooses Gelbooru, check if they want to change default URL
 if booru_choice == 2:
-    print('Would you like to change the Gelbooru URL from the default(gelbooru.com)? (Y/n):')
+    print('Would you like to change the Gelbooru URL from the default(https://gelbooru.com)? (Y/n):')
     url_choice = input()
     if url_choice.upper() == 'Y':
         print("Please type a URL: ")
         gelbooru_url = input()
     elif url_choice.upper() == 'N':
         gelbooru_url = 'https://gelbooru.com/'
+
+# If user chooses old Gelbooru, check if they want to change default URL
+elif(booru_choice == 3):
+    print('Would you like to change the Gelbooru URL from the default(https://rule34.xxx/)? (Y/n):')
+    url_choice = input()
+    if url_choice.upper() == 'Y':
+        print("Please type a URL: ")
+        gelbooru_url_old = input()
+    elif url_choice.upper() == 'N':
+        gelbooru_url_old = 'https://rule34.xxx/'
 
 # Check if images are NSFW
 print('Are your images NSFW? (Y/n): ')
@@ -82,7 +116,11 @@ while True:
     elif booru_choice == 2:
         print('Enter a image ID for Gelbooru:')
         id = input()
-        post = get_image_gelbooru(id)
+        post = get_image_gelbooru(id, gelbooru_url)
+    elif booru_choice == 3:
+        print('Enter a image ID for Gelbooru:')
+        id = input()
+        post = get_image_gelbooru_old(id, gelbooru_url_old)
 
     # Make request to download image
     image_response = requests.get(post['file_url'], headers=headers)
