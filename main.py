2d930f971aa5386fd13a305a13b0b408636c7d71
fuggy <45698918+nonetrix@users.noreply.github.com>
2023-01-22 01:28:31 -0600

    Seperate boorus into it's own file

--- main.py
+++ main.py
@@ -1,72 +1,8 @@
 import requests, os
+import boorus
 
 # User-Agent header for request
 headers = {'User-Agent': 'Booru2Training data a0.3'}
-
-def get_image_danbooru(id):
-    """
-    Get image details from Danbooru
-    :param id: Image ID on Danbooru
-    :return: Dictionary containing image details
-    """
-    # Make request to Danbooru API
-    response = requests.get('https://danbooru.donmai.us/posts/' + id + '.json', headers=headers)
-
-    # Check if request was successful
-    if response.status_code == 200:
-        post = response.json()
-        return {
-            'file_url': post['file_url'],
-            'file_ext': post['file_ext'],
-            'id': post['id'],
-            'tag_string': post['tag_string'],
-        }
-    else:
-        print('Request failed.')
-
-def get_image_gelbooru(id, url):
-    """
-    Get image details from Gelbooru
-    :param id: Image ID on Gelbooru
-    :param url: URL to instance
-    :return: Dictionary containing image details
-    """
-    # Make request to Gelbooru API
-    response = requests.get(url + '/index.php?page=dapi&s=post&q=index&json=1&id=' + id, headers=headers)
-
-    # Check if request was successful
-    if response.status_code == 200:
-        post = response.json()
-        return {
-            'file_url': post['post'][0]['file_url'],
-            'file_ext': post['post'][0]['file_url'][post['post'][0]['file_url'].rfind('.'):],
-            'id': post['post'][0]['id'],
-            'tag_string': post['post'][0]['tags'],
-        }
-    else:
-        print('Request failed.')
-
-def get_image_gelbooru_old(id, url):
-    """
-    Get image details from Gelbooru using older API
-    :param id: Image ID on Gelbooru
-    :param url: URL to instance
-    :return: Dictionary containing image details
-    """
-    # Make request to Gelbooru API
-    response = requests.get(url + '/index.php?page=dapi&s=post&q=index&json=1&id=' + id, headers=headers)
-
-    # Check if request was successful
-    if response.status_code == 200:
-        post = response.json()
-        return {
-            'file_url': post[0]['file_url'],
-            'file_ext': post[0]['file_url'][post[0]['file_url'].rfind('.'):],
-            'id': post[0]['id'],
-            'tag_string': post[0]['tags'],
-        }
-    else:
-        print('Request failed.')
 
 # Get directory to save images
 print('Pick a directory to save images to, it must already exist: ')
@@ -112,15 +48,15 @@ while True:
     if booru_choice == 1:
         print('Enter a image ID for Danbooru:')
         id = input()
-        post = get_image_danbooru(id)
+        post = boorus.get_image_danbooru(id, headers)
     elif booru_choice == 2:
         print('Enter a image ID for Gelbooru:')
         id = input()
-        post = get_image_gelbooru(id, gelbooru_url)
+        post = boorus.get_image_gelbooru(id, gelbooru_url, headers)
     elif booru_choice == 3:
         print('Enter a image ID for Gelbooru:')
         id = input()
-        post = get_image_gelbooru_old(id, gelbooru_url_old)
+        post = boorus.get_image_gelbooru_old(id, gelbooru_url_old, headers)
 
     # Make request to download image
     image_response = requests.get(post['file_url'], headers=headers)
